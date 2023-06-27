34d404953645ea885153f78ef36e5669fd34efd1
kylemann16 <kyle@hobu.co>
2023-06-27 16:40:35 -0500

    Remove old ami usage

--- resources/ec2.tf
+++ resources/ec2.tf
@@ -8,24 +8,9 @@ variable instance_type {
     default = "g4dn.4xlarge"
 }
 
-data aws_ami al2 {
-    most_recent = true
-
-    filter {
-        name   = "owner-alias"
-        values = ["amazon"]
-    }
-
-    filter {
-        name   = "name"
-        values = ["amzn2-ami-hvm*"]
-    }
-}
-
 data "aws_ssm_parameter" "ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/gpu/recommended"
 }
-
 
 resource aws_instance instance {
     instance_type = var.instance_type
